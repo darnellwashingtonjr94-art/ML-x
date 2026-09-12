d0aeef01a9d548fa82eac618c41024d43d62b810
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-09-12 16:43:15 -0700

    Create ecs.tf

---
+++ terraform/ecs.tf
@@ -0,0 +1,20 @@
+resource "aws_ecs_cluster" "mlx_cluster" {
+  name = var.cluster_name
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "mlx_cluster_providers" {
+  cluster_name = aws_ecs_cluster.mlx_cluster.name
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
+  }
+}
